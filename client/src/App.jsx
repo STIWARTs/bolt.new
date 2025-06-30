14a84070f0bbc2610a349ac5f690e1229e4f527d
STIWART STANCE SAXENA <138205066+STIWARTs@users.noreply.github.com>
2025-06-30 22:15:34 +0530

    Fix animated background z-index to stay behind components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,29 +8,35 @@ function App() {
   const {isLoggedIn} = useContext(GameContext)
   return (
     <div className='container'>
-      {/* Floating animated elements */}
-      <div className="floating-element pencil-1"></div>
-      <div className="floating-element brush-1"></div>
-      <div className="floating-element palette-1"></div>
-      <div className="floating-element star-1"></div>
-      <div className="floating-element star-2"></div>
-      <div className="floating-element doodle-1"></div>
-      
-      {/* Floating bubbles */}
-      <div className="floating-bubble bubble-1"></div>
-      <div className="floating-bubble bubble-2"></div>
-      <div className="floating-bubble bubble-3"></div>
-      <div className="floating-bubble bubble-4"></div>
-      
-      {/* Color orbs */}
-      <div className="color-orb orb-1"></div>
-      <div className="color-orb orb-2"></div>
-      <div className="color-orb orb-3"></div>
-      
-      {isLoggedIn?
-      <GamePage />:
-      <HomePage />
-      }
+      {/* Background animations layer - behind everything */}
+      <div className="background-animations">
+        {/* Floating animated elements */}
+        <div className="floating-element pencil-1"></div>
+        <div className="floating-element brush-1"></div>
+        <div className="floating-element palette-1"></div>
+        <div className="floating-element star-1"></div>
+        <div className="floating-element star-2"></div>
+        <div className="floating-element doodle-1"></div>
+        
+        {/* Floating bubbles */}
+        <div className="floating-bubble bubble-1"></div>
+        <div className="floating-bubble bubble-2"></div>
+        <div className="floating-bubble bubble-3"></div>
+        <div className="floating-bubble bubble-4"></div>
+        
+        {/* Color orbs */}
+        <div className="color-orb orb-1"></div>
+        <div className="color-orb orb-2"></div>
+        <div className="color-orb orb-3"></div>
+      </div>
+
+      {/* Main content layer - above background */}
+      <div className="main-content">
+        {isLoggedIn?
+        <GamePage />:
+        <HomePage />
+        }
+      </div>
     </div>
   );
 }
