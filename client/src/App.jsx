061e47172f9f606ea6bd432d92704ca9cdc0e72e
STIWART STANCE SAXENA <138205066+STIWARTs@users.noreply.github.com>
2025-06-30 22:10:19 +0530

    Add animated moveable background with floating elements

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,12 +8,30 @@ function App() {
   const {isLoggedIn} = useContext(GameContext)
   return (
     <div className='container'>
+      {/* Floating animated elements */}
+      <div className="floating-element pencil-1"></div>
+      <div className="floating-element brush-1"></div>
+      <div className="floating-element palette-1"></div>
+      <div className="floating-element star-1"></div>
+      <div className="floating-element star-2"></div>
+      <div className="floating-element doodle-1"></div>
+      
+      {/* Floating bubbles */}
+      <div className="floating-bubble bubble-1"></div>
+      <div className="floating-bubble bubble-2"></div>
+      <div className="floating-bubble bubble-3"></div>
+      <div className="floating-bubble bubble-4"></div>
+      
+      {/* Color orbs */}
+      <div className="color-orb orb-1"></div>
+      <div className="color-orb orb-2"></div>
+      <div className="color-orb orb-3"></div>
+      
       {isLoggedIn?
       <GamePage />:
       <HomePage />
       }
     </div>
-
   );
 }
 
